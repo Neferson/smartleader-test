044ec7dd6d7383ddf6c2bb5c018e9fabfd024963
Neferson Oliveira <tjnef@hotmail.com>
2023-04-14 09:28:13 -0300

    Inserida as cores de background do card - tema escuro

--- src/common/components/CardComponent.vue
+++ src/common/components/CardComponent.vue
@@ -27,26 +27,6 @@
 
 <style lang="scss" scoped>
 
- .card {
-    background-color: var(--card-background);
-    max-width: 100%;
-    border-radius: 12px;
-    box-shadow: 0px 1px 3px rgba(16, 24, 40, 0.1), 0px 1px 2px rgba(16, 24, 40, 0.06);
-
-    .default{
-      padding: 13px 21px;
-    }
-    
-    .header {
-      border-bottom: 1px solid var(--card-border-color);
-      display: flex;
-      justify-content: space-between;
-      padding: 13px 21px;
-
-      font-weight: 600;
-      font-size: 16px;
-      color: #4E5D78;
-    }
- }
+ 
  
 </style>
